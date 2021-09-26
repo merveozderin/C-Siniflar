8e562b35c81478c50eccae4408a0d6044cd187bc
Merve <61613791+merveozderin@users.noreply.github.com>
2021-09-26 19:26:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Sınıf_Kavramı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan() ;
+            calisan1.Ad = "Merve" ;
+            calisan1.Soyad = "Özderin" ;
+            calisan1.No = 12345678 ;
+            calisan1.Departman = "ARGE" ;
+
+            Calisan calisan2 = new Calisan() ;
+            calisan2.Ad = "Deniz" ;
+            calisan2.Soyad = "Gökyüzü" ;
+            calisan2.No = 87654321 ;
+            calisan2.Departman = "İnsan Kaynakları" ;
+
+            calisan1.CalisanBilgileri() ;
+            Console.WriteLine("**************") ;
+            calisan2.CalisanBilgileri() ;
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışanın Adı:{0}", Ad) ;
+            Console.WriteLine("Çalışanın Soyadı:{0}", Soyad) ;
+            Console.WriteLine("Çalışanın Numarası:{0}", No) ;
+            Console.WriteLine("Çalışanın Departmanı:{0}", Departman) ;
+        }
+
+    }
+}
